f88cdd7241b41e24e10f9c6270dea6880592e062
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-10-14 18:01:36 +0200

    Update producttb.sql

--- Shopping/producttb.sql
+++ Shopping/producttb.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 09, 2021 at 04:23 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `productdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `producttb`
---
 
 CREATE TABLE `producttb` (
   `id` int(11) NOT NULL,
@@ -34,9 +12,6 @@ CREATE TABLE `producttb` (
   `product_image` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `producttb`
---
 
 INSERT INTO `producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
 (1, 'Coat', 450, './upload/product1.png'),
@@ -44,27 +19,11 @@ INSERT INTO `producttb` (`id`, `product_name`, `product_price`, `product_image`)
 (3, 'Purse', 459, './upload/product3.png'),
 (4, 'T shirt', 278, './upload/product4.png');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `producttb`
---
 ALTER TABLE `producttb`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `producttb`
---
 ALTER TABLE `producttb`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
